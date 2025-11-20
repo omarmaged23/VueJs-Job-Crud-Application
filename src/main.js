5b59d42351d6c33e7c4e73a6eefa0342ad5a143f
omarmaged23 <omarmaged292@gmail.com>
2025-11-20 20:42:18 +0200

    release

---
+++ src/main.js
@@ -0,0 +1,13 @@
+import { createApp } from 'vue'
+import '@/assets/main.css';
+import 'primeicons/primeicons.css'
+import Toast from 'vue-toastification'
+import 'vue-toastification/dist/index.css'
+import App from './App.vue'
+import router from './router'
+
+const app = createApp(App);
+
+app.use(router);
+app.use(Toast)
+app.mount('#app');
